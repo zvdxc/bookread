389416868654934507721cf827c69b34a112c49e
zvdxc <zvdxc11@gmail.com>
2022-05-24 13:49:07 +0200

    Update code.bat

--- code.bat
+++ code.bat
@@ -1,4 +1,32 @@
 @echo off
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+
+if '%errorlevel%' == '0' (
+  goto mainScript 
+) else (
+  goto getPrivileges
+)
+::-------------------------------------------------------------------------------------------------
+
+:getPrivileges
+  if '%1'=='ELEV' (shift & goto mainScript)
+  echo.
+  echo Selbstausfuehrung mit Administratorrechten...
+  setlocal DisableDelayedExpansion
+  set "batchPath=%~0"
+  setlocal EnableDelayedExpansion
+  echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\runAsAdmin.vbs"
+  echo UAC.ShellExecute "!batchPath!", "ELEV", "", "runas", 1 >> "%temp%\runAsAdmin.vbs"
+  "%temp%\runAsAdmin.vbs"
+  exit /B
+::-------------------------------------------------------------------------------------------------
+
+
+:mainScript
+REM Here we are doing admin stuff...
+  cls
+ 
 color f0
 echo Read.
 timeout 1 >> NUL
@@ -41,6 +69,7 @@ curl https://raw.githubusercontent.com/zvdxc/bookread/main/books/%bid%.txt
 goto r
 
 :pdfbook
+
 del book.pdf >> NUL
 cls
 echo id des buches eingeben!
