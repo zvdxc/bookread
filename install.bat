669827287374f40f48b789da2f14972fc775c077
zvdxc <88501710+zvdxc@users.noreply.github.com>
2022-05-25 19:40:29 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,13 +1,19 @@
 rd /S /Q BookRead
 md BookRead
 cd BookRead
+md sources
+cd sources
 curl https://raw.githubusercontent.com/zvdxc/bookread/main/sources/code.bat >> code.bat
 curl https://raw.githubusercontent.com/zvdxc/bookread/main/sources/startvbs.bat >> startvbs.bat
 curl https://raw.githubusercontent.com/zvdxc/bookread/main/sources/audiocrowd_free_track_37_funkadelic.mp3 >> audiocrowd_free_track_37_funkadelic.mp3
 curl https://raw.githubusercontent.com/zvdxc/bookread/main/sources/.vbs >> .vbs
-
+cd..
 curl https://raw.githubusercontent.com/zvdxc/bookread/main/sources/uninstall.bat >> uninstall.bat
-echo. >> run the code.bat to start
+echo cd sources >> start.bat
+echo start code.bat >> start.bat
+echo exit >> start.bat
+
+
 cd..
 
 pause
