770042a0c1ac9b468992c56f626bf5cf98c95c14
zvdxc <zvdxc11@gmail.com>
2022-05-27 13:30:49 +0200

    Update uninstall.bat

--- sources/uninstall.bat
+++ sources/uninstall.bat
@@ -1,9 +1,8 @@
 cd..
 curl https://raw.githubusercontent.com/zvdxc/bookread/main/sources/uninstall.ps1 >> uninstall.ps1
-echo powershell .\uninstall.ps1 >> OHNO.bat
-start OHNO.bat
-timeot 2 >> NUL
-del OHNO.bat
+powershell .\uninstall.ps1
+
+
 del uninstall.ps1
 rd /S /Q BookRead
 
