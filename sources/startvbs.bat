31f480ce5a9d26f2a8583728c7903dc481c8c08c
zvdxc <zvdxc11@gmail.com>
2022-05-30 13:53:40 +0200

    Update startvbs.bat

--- sources/startvbs.bat
+++ sources/startvbs.bat
@@ -68,12 +68,13 @@ echo "| _ \/ _ \/ _ \| / /|   // -_)/ _` |/ _` |"
 echo "|___/\___/\___/|_\_\|_|_\\___|\__/_|\__/_|"
 echo.
 echo.
-echo 1 pdf buch lesen 2 txt buch lesen 3 index ansehen e Beenden
-choice /c 123e
+echo 1 pdf buch lesen 2 txt buch lesen 3 index ansehen 4 epub lesen e Beenden
+choice /c 1234e
 if %errorlevel% == 1 goto pdfbook
 if %errorlevel% == 2 goto txtbook
 if %errorlevel% == 3 goto index
-if %errorlevel% == 4 goto end
+if %errorlevel% == 4 goto epub
+if %errorlevel% == 5 goto end
 :txtbook
 echo id des buches eingeben!
 set /p bid=id: 
@@ -180,3 +181,14 @@ echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%ra
 echo %random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%%random%
 timeout 1 >> nul
 exit
+
+
+
+:epub
+cls
+echo id des buches eingeben!
+set /p bid=id: 
+cls
+del book.epub
+curl https://raw.githubusercontent.com/zvdxc/bookread/main/books/%bid%.epub >> book.epub
+start book.epub
